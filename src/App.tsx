14aac8831137f641245812869cb3f8f1f8cdc4f0
Omurbekova Tolkun <toprog27@gmail.com>
2023-04-04 16:04:56 +0600

    -added router for profile and dialogs pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import TodoList from "./TodoList";
+import {Todolist} from './Todolist';
 
-export type TaskType={
-    id:number,
-    title:string,
-    isDone:boolean
-}
-function App():JSX.Element   {
-    const tasks:Array<TaskType>=[
-        {id:1,title:'HTML & CSS',isDone:true,},
-        {id:2,title:'CSS & SCC',isDone:true,},
-        {id:3,title:'ES6 & TS',isDone:false,}
+export type filterKey = 'All' | 'Active' | 'Completed';
+
+function App() {
+
+    let originalTasks = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
     ]
+    let [tasks, setTasks] = useState(originalTasks);
+
+    // const tasks2 = [
+    //     { id: 1, title: "Hello world", isDone: true },
+    //     { id: 2, title: "I am Happy", isDone: false },
+    //     { id: 3, title: "Yo", isDone: false }
+    // ]
+    const removeTask = (taskId: Number) => {
+        // tasks = tasks.filter((el) => {
+        //     el.id !== taskId
+        // })
+        setTasks(tasks.filter(el => {
+            el.id !== taskId
+        }));
+    }
+    // let[globallFilterKey,setgloballFilterKey]=useState('All');
+    // const tasksFilter = (filterKey: filterKey) => {
+    //     setgloballFilterKey(filterKey);
+    // }
+
+
     return (
         <div className="App">
-            <TodoList title={"What to learn"} tasks={tasks}/>
-            <TodoList title={"What to buy"} tasks={tasks}/>
-            <TodoList title={"What to read"} tasks={tasks}/>
+            <Todolist
+                title="What to learn"
+                tasks={tasks}
+                removeTask={removeTask}
+            />
+            {/*<Todolist title="Songs" tasks={tasks2} />*/}
         </div>
     );
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,35 +1,68 @@
-import React, {FC} from 'react';
-import {TaskType} from "./App"
-type TodoListPropsType={
-    title:string,
-    tasks:Array<TaskType>
-}
-const TodoList:FC<TodoListPropsType> = (props:TodoListPropsType) => {
-    return (
-        <div className="todolist">
-            <h3>{props.title}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li><input type="checkbox" checked={true}/>
-                    <span>{props.tasks[0].title}</span>
-                </li>
-                <li><input type="checkbox" checked={true}/>
-                    <span>{props.tasks[1].title}</span>
-                </li>
-                <li><input type="checkbox" checked={false}/>
-                    <span>props.tasks[2].title</span>
-                </li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
-        </div>
-    );
-};
+import React, {useState} from 'react';
+import {filterKey} from './App';
 
-export default TodoList;
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (taskId: number) => void;
+    //tasksFilter: (filter: filterKey, taskArr:Array<TaskType>) => void;
+}
+let [globalFilterKey, setGlobalFilterKey] = useState('All')
+const tasksFilter = (filterKey: filterKey) => {
+    //засетай filterKey в globalFilterKey
+    setGlobalFilterKey(filterKey)
+}
+
+
+
+export function Todolist(props: PropsType) {
+    let collander = props.tasks;
+    if (globalFilterKey === 'Active') {
+        collander = props.tasks.filter(el => !el.isDone)
+    }
+    if (globalFilterKey === 'Completed') {
+        collander = props.tasks.filter(el => el.isDone)
+    }
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            {collander.map(el => {
+                return (
+                    <li key={el.id}>
+                        <input type="checkbox" checked={el.isDone}/> <span>{el.title}</span>
+                        <button onClick={() => {
+                            props.removeTask(el.id)
+                        }}></button>
+                    </li>
+                )
+            })}
+
+            {/*<li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>*/}
+            {/*<li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>*/}
+        </ul>
+        <div>
+            <button onClick={() => {
+                tasksFilter('All')
+            }}>All
+            </button>
+            <button onClick={() => {
+                tasksFilter('Active')
+            }}>Active
+            </button>
+            <button onClick={() => {
+                tasksFilter('Completed')
+            }}>Completed
+            </button>
+        </div>
+    </div>
+}
