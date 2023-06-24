7d8096f3b1c199d3f43407322aec6cad0d7084dc
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-06-24 13:48:17 +0600

    -added todolistApi

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {CreateTodolist, DeleteTodolist, GetTodolists} from '../stories/todolists-api.stories';
 
 const instance = axios.create({
     baseURL:'https://social-network.samuraijs.com/api/1.1/',
@@ -7,11 +8,21 @@ const instance = axios.create({
 })
 
 export const todolistAPI = {
+    getTodolists(){
+        return instance.get(`todo-lists`);
+    },
+    createTodolist(title:string){
+        return instance.post('todo-lists',{title});
+    },
+    deleteTodolist(todolistId:string){
+        return instance.delete(`todo-lists/${todolistId}`);
+    },
     updateTodolist(todolistId: string, title: string) {
         const promise = instance.put(
             `todo-lists/${todolistId}`,
             { title: title },
         )
         return promise
-    },
+    }
+
 }
